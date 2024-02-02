0f25dea2ac8be7972719270a6308e2f7bdc8fcac
William Blake Galbreath <Blake.Galbreath@GMail.com>
2024-02-01 21:19:20 -0600

    v1.0.5

--- src/TweaksMod.cs
+++ src/TweaksMod.cs
@@ -107,27 +107,28 @@ public sealed partial class TweaksMod : ModSystem {
                 .WithDescription("")
                 .RequiresPrivilege(Privilege.chat)
                 .HandleWith(_ => {
-                    TemporalStormRunTimeData data = api.ModLoader.GetModSystem<SystemTemporalStability>().StormData;
                     string message;
                     double days;
+
+                    TemporalStormRunTimeData data = api.ModLoader.GetModSystem<SystemTemporalStability>().StormData;
                     if (data.nowStormActive) {
-                        message = "{0} temporal storm is still active for ";
+                        message = $"{data.nextStormStrength} temporal storm is still active for ";
                         days = data.stormActiveTotalDays - api.World.Calendar.TotalDays;
                     } else {
                         message = "Next temporal storm is in ";
                         days = data.nextStormTotalDays - api.World.Calendar.TotalDays;
                     }
 
-                    if (days > 0) {
+                    double hours = days * 24 % 24;
+                    double minutes = hours * 60 % 60;
+
+                    if ((int)days > 0) {
                         message += "{0:day;days}, {1:hour;hours}, and {2:minute;minutes}";
-                    } else if (days > 0) {
+                    } else if ((int)hours > 0) {
                         message += "{1:hour;hours} and {2:minute;minutes}";
                     } else {
                         message += "{2:minute;minutes}";
                     }
-
-                    double hours = days * 24 % 24;
-                    double minutes = hours * 60 % 60;
 
                     return TextCommandResult.Success(message.Format((int)days, (int)hours, (int)minutes));
                 });
