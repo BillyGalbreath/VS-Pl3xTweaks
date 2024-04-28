5e55aa576f33de019aed3830c976c2b9f0d336a5
BillyGalbreath <blake.galbreath@gmail.com>
2024-04-28 17:54:16 -0500

    v1.1.21

--- src/Patches/ServerHarmonyPatches.cs
+++ src/Patches/ServerHarmonyPatches.cs
@@ -4,9 +4,10 @@ namespace Pl3xTweaks.Patches;
 
 public sealed class ServerHarmonyPatches : HarmonyPatches {
     public ServerHarmonyPatches(TweaksMod mod) : base(mod) {
-        _ = new BlockClutterPatches(Harmony);
+        //_ = new BlockClutterPatches(Harmony); // 1.19.4 broke this
         _ = new BlockEntityCharcoalPitPatches(Harmony);
         _ = new BlockEntityCoalPilePatches(Harmony);
+        _ = new BlockEntityPitKilnPatches(Harmony);
         _ = new EntityAgentPatches(Harmony);
         _ = new EntityBehaviorCollectEntitiesPatches(Harmony);
         _ = new FirepitPatches(Harmony);
