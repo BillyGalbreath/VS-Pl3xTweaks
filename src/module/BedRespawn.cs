bec337b66ca087b59a37c7b796b91ce56188d7ec
BillyGalbreath <blake.galbreath@gmail.com>
2024-05-18 16:49:26 -0500

    better bed respawn detection (including breaking bed)

--- src/module/BedRespawn.cs
+++ src/module/BedRespawn.cs
@@ -1,6 +1,7 @@
 using System.Text.RegularExpressions;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 using Vintagestory.GameContent;
 
@@ -18,22 +19,45 @@ public partial class BedRespawn : Module {
         api.Event.DidBreakBlock += OnBreakBlock;
     }
 
-    private void OnUseBlock(IServerPlayer player, BlockSelection block) {
-        if (_api.World.BlockAccessor.GetBlock(block.Position) is not BlockBed bedBlock) {
+    private void OnUseBlock(IServerPlayer player, BlockSelection blocksel) {
+        if (_api.World.BlockAccessor.GetBlock(blocksel.Position) is not BlockBed bedBlock) {
             return;
         }
         if (WoodBedsRegex().Matches(bedBlock.Code.Path).Count == 0) {
             return;
         }
-        if (player.GetSpawnPosition(false).AsBlockPos.Add(0, -1, 0) == block.Position) {
+        BlockPos pos = GetBedFeetPos(bedBlock, blocksel).AddCopy(0, 1, 0);
+        if (player.GetSpawnPosition(false).AsBlockPos == pos) {
             return;
         }
-        player.SetSpawnPosition(new PlayerSpawnPos(block.Position.X, block.Position.Y + 1, block.Position.Z));
+        player.SetSpawnPosition(new PlayerSpawnPos(pos.X, pos.Y, pos.Z));
         player.SendMessage(GlobalConstants.GeneralChatGroup, Lang.Get("pl3xtweaks:set-new-respawn-point"), EnumChatType.Notification);
     }
 
     private void OnBreakBlock(IServerPlayer player, int oldblockid, BlockSelection blocksel) {
-        //
+        if (_api.World.BlockAccessor.GetBlock(oldblockid) is not BlockBed bedBlock) {
+            return;
+        }
+        if (WoodBedsRegex().Matches(bedBlock.Code.Path).Count == 0) {
+            return;
+        }
+        BlockPos pos = GetBedFeetPos(bedBlock, blocksel).AddCopy(0, 1, 0);
+        string self = Lang.Get("pl3xtweaks:cleared-respawn-point");
+        string other = Lang.Get($"pl3xtweaks:cleared-respawn-point-by-other", player.PlayerName);
+        foreach (IServerPlayer offline in (IServerPlayer[])_api.World.AllPlayers) {
+            if (offline.GetSpawnPosition(false).AsBlockPos == pos) {
+                offline.ClearSpawnPosition();
+                offline.SendMessage(GlobalConstants.GeneralChatGroup, offline.PlayerUID == player.PlayerUID ? self : other, EnumChatType.Notification);
+            }
+        }
+    }
+
+    private static BlockPos GetBedFeetPos(BlockBed bedBlock, BlockSelection blocksel) {
+        BlockPos? pos = blocksel.Position.Copy();
+        if (bedBlock.LastCodePart(1) != "feet") {
+            pos.Add(BlockFacing.FromCode(bedBlock.LastCodePart()));
+        }
+        return pos;
     }
 
     public override void Dispose() {
